743645d12e511e900385392b74ec6bc73de8ca9b
dporraws <104505109+dporraws@users.noreply.github.com>
2023-12-15 22:07:08 +0000

    Reply with PONG

--- app/server.c
+++ app/server.c
@@ -3,6 +3,24 @@
 #include <sys/socket.h>
 #include <errno.h>
 #include <netinet/in.h>
+#include <regex.h>     
+
+/* Parses a line in the form *N\r\n$M\r\nABC...M\r\nEOF
+returns an array of N elements containing the command and its arguments
+ie. 
+*1\r\n$4\r\nping\r\n
+["PING"]
+*/
+// char* parse_line(char* command){
+// 	char* cmd;
+// 	int c;
+// 	for(int i=0; c != 'EOF'; i++)
+
+// 	return cmd;
+// }
+
+// void handle_PING(char* arguments,char* reply){
+// }
 
 int main() {
 	// Disable output buffering
@@ -12,6 +30,13 @@ int main() {
 	printf("Logs from your program will appear here!\n");
 
 	int socket_fd = socket(PF_INET, SOCK_STREAM, 0);
+	//Since the tester restarts your program quite often, setting REUSE_PORT
+	//ensures that we don't run into 'Address already in use' errors
+	int reuse = 1;
+	if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
+		printf("SO_REUSEPORT failed: %s \n", strerror(errno));
+		return 1;
+	}
 	// int bind(int socket, const struct sockaddr *address, socklen_t address_len);
 	struct sockaddr_in  srv_addr;
 	memset(&srv_addr, 0, sizeof(struct sockaddr_in));
@@ -25,7 +50,14 @@ int main() {
 
 	bind(socket_fd, (struct sockaddr *) &srv_addr, sizeof(srv_addr)); 
 	listen(socket_fd, 50);
-	accept(socket_fd,0,0);
+	uint32_t client_fd = accept(socket_fd,0,0);
+	uint32_t REQUEST_SIZE = 4096;
+	char* request[REQUEST_SIZE];
+	memset(&request, 0, REQUEST_SIZE * sizeof(char));
+	if(client_fd) recv(client_fd, &request, REQUEST_SIZE, 0 );
+	printf("%s", &request); //FIX: This generates a warning
+	char response[] = "+PONG\r\n";
+	send(client_fd, response,sizeof(response),0);
 	// Uncomment this block to pass the first stage
 	//
 	// int server_fd, client_addr_len;
@@ -37,13 +69,6 @@ int main() {
 	// 	return 1;
 	// }
 	//
-	// // Since the tester restarts your program quite often, setting REUSE_PORT
-	// // ensures that we don't run into 'Address already in use' errors
-	// int reuse = 1;
-	// if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
-	// 	printf("SO_REUSEPORT failed: %s \n", strerror(errno));
-	// 	return 1;
-	// }
 	//
 	// struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
 	// 								 .sin_port = htons(6379),
